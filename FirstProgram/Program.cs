af00fa8675cb81bddd01d3daf45fe1316add4850
AnaMK16 <you@example.com>
2023-06-01 21:13:58 +0400

    initial commit

---
+++ FirstProgram/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+ConsoleKeyInfo keyInfo = Console.ReadKey();
+Console.WriteLine("You pressed: " + keyInfo.KeyChar);
---
+++ FirstProject/Program.cs
@@ -0,0 +1,21 @@
+﻿// Comments
+//Variables
+
+//Data types
+//Console.WriteLine("Enter first Number : ");
+//int firstNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter Second Number : ");
+//int secondNumber = int.Parse(Console.ReadLine());
+//int thirdNumber = firstNumber + secondNumber;
+//Console.WriteLine($"Sum of {firstNumber} and {secondNumber} is {thirdNumber}");
+
+//convert
+long number = 5;
+int number1 = Convert.ToInt32(number);
+//Console.WriteLine(number1);
+
+
+int number2 = 6;
+number2 = 10;
+Console.WriteLine(number2);
+    
