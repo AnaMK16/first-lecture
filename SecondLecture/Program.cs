9f8fd4062b8bebeba6aa4ee438a40de244d794f7
AnaMK16 <you@example.com>
2023-06-03 14:34:44 +0400

    Added second lecture

---
+++ SecondLecture/Program.cs
@@ -0,0 +1,35 @@
+﻿//operators
+int firstNumber = -5;
+
+
+//Console.WriteLine(firstNumber);
+//strings
+string name = "ana";
+if (string.IsNullOrWhiteSpace(name))
+{
+   // Console.WriteLine("name is empty");
+}
+//Math
+
+int secondNumber = 27;
+//Console.WriteLine(Math.Pow(secondNumber, 2));
+string result = (firstNumber > 0) ? "number is positive" : "number is negative";
+//Console.WriteLine(result);
+
+if (firstNumber != 2)
+{
+ // Console.WriteLine("number does not equal to 2");
+}
+else if (firstNumber == 2)
+{
+   // Console.WriteLine("number equals to 2");
+}
+
+int number = 1;
+
+
+while (true)
+{
+    Console.WriteLine(number);
+    number++;
+}
